8cf743889ebe3072668fb5a84245ccf4d40445f6
luciferwithjupiter <lifeonjupiterbylucifer@gmail.com>
2024-06-21 18:28:47 -0700

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "16494736"
 api_hash = "2cb7fa5859e2de684e3e10d9c049621a"
-bot_token = "6975437554:AAHcY-8pF40DREoGJGf91ZHFk2skVTj6kZo"
+bot_token = "6807963658:AAFaK_su2q-6VTaMvaacBdkeP1CmqPV6afM"
